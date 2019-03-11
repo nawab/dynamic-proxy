78306948758e05e320e217f9dbe9b94b7b7cc581
vivekchaturvedi <vchaturvedi@equalexperts.com>
2019-03-11 11:40:30 +0530

    mapping response for performance matrix

--- src/main/java/ultimate/proxy/api/model/PerformanceTransactionResponse.java
+++ src/main/java/ultimate/proxy/api/model/PerformanceTransactionResponse.java
@@ -3,11 +3,31 @@ package ultimate.proxy.api.model;
 import lombok.Data;
 import ultimate.proxy.domain.Transaction;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Data
 public class PerformanceTransactionResponse {
-    public static PerformanceTransactionResponse from(String key, List<Transaction> value) {
-        return null;
+    private final String serviceName;
+    private final List<HashMap> transactions;
+
+    public PerformanceTransactionResponse(String serviceName, List<HashMap> transactions) {
+        this.serviceName = serviceName;
+        this.transactions = transactions;
+    }
+
+    public static PerformanceTransactionResponse from(String serviceName, List<Transaction> transactions) {
+        List<HashMap> performanceMatrix = transactions.stream()
+                .map(PerformanceTransactionResponse::mapMatrix)
+                .collect(Collectors.toList());
+        return new PerformanceTransactionResponse(serviceName, performanceMatrix);
+    }
+
+    private static HashMap mapMatrix(Transaction transaction) {
+        HashMap matrix = new HashMap();
+        matrix.put("executionTime", transaction.getExecutionTime());
+        matrix.put("startTime", transaction.getStartTime());
+        return matrix;
     }
 }
